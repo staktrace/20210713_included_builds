9958caa559350544058b0ca67f28b5694e22c9a3
Kartikaya Gupta <kartikaya@squareup.com>
2021-07-13 14:36:38 -0400

    Do top-level dependency substitution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,12 @@
 rootProject.name = "20210713_included_builds"
 
-includeBuild("project_a/")
-includeBuild("project_b/")
+includeBuild("project_a/") {
+  dependencySubstitution {
+    substitute(module("com.squareup.test.project_a:project_a")).with(project(":submodule"))
+  }
+}
+includeBuild("project_b/") {
+  dependencySubstitution {
+    substitute(module("com.squareup.test.project_b:project_b")).with(project(":submodule"))
+  }
+}
